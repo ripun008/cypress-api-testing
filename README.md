7cabf759f5ed5b4db8d0a1eb56c67155e8d14bfc
Ripunjaya Saran <ripun008@gmail.com>
2024-10-22 19:38:11 -0400

    add test for request & response assertions

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Cypress API testing
 
 - npm init (to initialize npm project)
 - npm install cypress --save-dev
+- add /// <reference types="cypress" /> to individual files or add a global jsoncong.json file
--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,4 +1,3 @@
-/// <reference types="cypress" />
 
 describe('Cypress API tests', () => {
 
@@ -6,7 +5,33 @@ describe('Cypress API tests', () => {
     cy.loginToApplication()
   }) 
 
-  it('login via backend', () => {
-    cy.log('logged in to the app!')
+  it('verify correct request & response', () => {
+    cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/').as('postArticles')
+
+    cy.contains('New Article').click()
+    cy.get('[placeholder="Article Title"]').click().type('test article title')
+    cy.get('[formcontrolname="description"]').click().type('test article description')
+    cy.get('[placeholder="Write your article (in markdown)"]').click().type('test article body')
+    cy.contains('Publish Article').click()
+
+     // wait for API call to finish
+    cy.wait('@postArticles').then( xhr => { 
+      console.log(xhr)
+      
+      // request assertions
+      expect(xhr.request.body.article.body).to.equal('test article body')
+      expect(xhr.request.body.article.description).to.equal('test article description')
+      expect(xhr.request.body.article.title).to.equal('test article title')
+      expect(xhr.request.method).to.equal('POST')
+
+      // response assertions
+      expect(xhr.response.statusCode).to.equal(201)
+      expect(xhr.response.body.article.author.username).to.equal('mwaugh8')
+      expect(xhr.response.body.article.body).to.equal('test article body')
+      expect(xhr.response.body.article.description).to.equal('test article description')
+      expect(xhr.response.body.article.title).to.equal('test article title')
+      
+
+    })
   })
 })
