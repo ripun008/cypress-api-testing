1a1805ad544e743e9025691f73f835ae07085b36
Ripunjaya Saran <ripun008@gmail.com>
2024-10-23 21:28:45 -0400

    more mocked tests

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -2,6 +2,7 @@
 describe('Cypress API tests', () => {
 
   beforeEach('login to Application', () => {
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/tags', {fixture: 'tags.json'})
     cy.loginToApplication()
   }) 
 
@@ -30,8 +31,34 @@ describe('Cypress API tests', () => {
       expect(xhr.response.body.article.body).to.equal('test article body')
       expect(xhr.response.body.article.description).to.equal('test article description')
       expect(xhr.response.body.article.title).to.equal('test article title')
-      
-
     })
+  })
+
+  // mocking a response
+  it('verify popular tags are displayed', () => {
+    cy.get('.tag-list')
+    .should('contain', 'cypress')
+    .and('contain', 'automation')
+    .and('contain', 'testing')
+  })
+
+  it.only('verify global feed likes count', () => {
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles/feed*', '{"articles":[],"articlesCount":0}')
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles*', { fixture: 'articles.json'})
+    
+
+    cy.contains('Global Feed').click()
+    cy.get('app-article-list button').then( heartList => {
+      expect(heartList[0]).to.contain('1')
+      expect(heartList[1]).to.contain('5')
+    })
+
+    cy.fixture('articles.json').then( file => {
+      const articleLink = file.articles[1].slug
+      file.articles[1].favoritesCount = 6
+      cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/'+articleLink+'/favorite', file)
+    })
+
+    cy.get('app-article-list button').eq(1).click().should('contain', '6')
   })
 })
